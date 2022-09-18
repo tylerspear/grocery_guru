60e41758d3d8b4d6d0f6f4f4c73cad90877f7618
Tyler Spear <stylerspear@gmail.com>
2022-09-17 22:18:32 -0500

    added add to cart route

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,5 +1,8 @@
 module.exports = {
    getCart: async (req, res) => {
     res.render('cart', {title: 'Cart'})
+   },
+   addToCart: async (req, res) => {
+      res.redirect('back')
    }
 }
--- routes/cart.js
+++ routes/cart.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const cartController = require('../controllers/cart')
 const { ensureAuth } = require('../middleware/auth')
 router.get('/', ensureAuth, cartController.getCart)
-
+router.put('/add/:id', ensureAuth, cartController.addToCart)
 module.exports = router
